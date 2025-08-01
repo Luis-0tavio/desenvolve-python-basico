bf2b7b92ad7bcead330e557ef6e1ef32e8083b14
Luis-0tavio <luisotavio.azevedopdita879@pditabira.com>
2025-08-01 11:27:29 -0300

    Add files via upload

---
+++ Módulo-2/aula2_questao1.py
@@ -0,0 +1,6 @@
+n1=5
+n2=7
+print(type(n1))
+print(type(n2))
+n3=n1/n2
+print(type(n3))
---
+++ Módulo-2/aula2_questao2.py
@@ -0,0 +1,12 @@
+v1="o resultado é"
+v2=10
+v3=3.5
+v4=v2+v3
+print(v1)
+print(type(v1))
+print(v2)
+print(type(v2))
+print(v3)
+print(type(v3))
+print(v4)
+print(type(v4))
